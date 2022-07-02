4baff4277e3f923ab48552e4cfec574f75504314
Kidobashi <daryldon1abril@gmail.com>
2022-07-02 15:35:10 +0800

    29th commit

--- resources/views/partials/forward.blade.php
+++ resources/views/partials/forward.blade.php
@@ -5,7 +5,7 @@
     @csrf
     <label for="">Forward to:</label>
         <div class="mb-3">
-            <input type="text" class="form-control" name="prevReceiver" id="name" value="{{ $doc->receiverName }}"aria-label="Name" aria-describedby="name" style="display: block;">
+            <input type="text" class="form-control" name="prevReceiver" id="name" value="{{ $doc->receiverName }}"aria-label="Name" aria-describedby="name" style="display: none;" disabled>
             <input type="text" class="form-control" name="receiverName" id="name" value="{{ $doc->receiverName }}"aria-label="Name" aria-describedby="name">
             @error('receiverName')
                 <p class="text-danger text-xs mt-2">{{ $message }}</p>
--- resources/views/users/qrinfo.blade.php
+++ resources/views/users/qrinfo.blade.php
@@ -25,72 +25,92 @@ ul:first-child{
     color: red;
     background: aquamarine;
 }
+.float-container {
+    border: 3px solid #fff;
+    padding: 20px;
+}
+
+.float-child {
+    float: left;
+    padding: 20px;
+}
+
 
 </style>
 
     @include('layouts.navbars.auth.nav')
-        <div class="row">
-            <div class="col-md-3">
-                    <div class="card-body my-auto" style="margin-left: 12px; border: 1px solid black">
-                        <h4 class="card-title">Document Details</h4>
-                        <hr>
-                        <h5>Reference No.: {{$data->referenceNo}}</h5>
-                        <h5>Sender: {{$data->senderName}}</h5>
+    <div class="container" style="position: absolute; right:4%;">
+        <div class="row g-3">
+                <div class="col-xs-7 col-sm-6 col-lg-8">
+                    <div class="d-flex float-container">
+                        <div class="float-child">
+                        <div class="d-flex flex-column" style="width: 100%;">
+                            <h4 class="card-title">Document Details</h4>
+                            <hr>
+                            <h5>Reference No.: {{$data->referenceNo}}</h5>
+                            <h5>Sender: {{$data->senderName}}</h5>
 
-                        <h5>From Office: {{$data->officeName}}</h5>
+                            <h5>From Office: {{$data->officeName}}</h5>
 
-                        <Label>Options</Label>
-                        @if($light->action == 2)
-                            <button type="button" class="btn btn-primary" disabled><a href="{{ url('forward/'.$data->referenceNo) }}">Forward</a></button>
-                            <button type="button" class="btn btn-success"><a href="{{ url('receive/'.$data->referenceNo) }}">Receive</a></button>
-                        @endif
-                        @if($light->action == 1)
-                            <button type="button" class="btn btn-primary"><a href="{{ url('forward/'.$data->referenceNo) }}">Forward</a></button>
-                            <button type="button" class="btn btn-success" disabled><a href="{{ url('receive/'.$data->referenceNo) }}">Receive</a></button>
-                        @endif
-                    </div>
-                    <div class="">
-                        <img src="{{ asset('qrcodes/qr'.$data->referenceNo.'.png') }}" alt="tag" style="margin-left:160px;">
-                    </div>
-            </div>
+                            <div class="" style="">
+                                <img src="{{ asset('qrcodes/qr'.$data->referenceNo.'.png') }}" alt="tag" style="margin-left:160px;">
+                            </div>
 
-            <div class="col-md-6">
-                <h3>TRACKING</h3>
-                <div class="card" style="width: 30rem;">
-                    @foreach($altdata['prev'] as $key => $prev)
-                    <ul class="list-group list-group-flush">
-                    <div class="section-header">
-                        <li>
-                        @if( $altdata['trackings'][$key]->action == 1)
-                        <h5>Received by {{ $altdata['trackings'][$key]->receiverName }}</h5>
-                            <li class="list-group-item">Office: <i>{{ $altdata['trackings'][$key]->officeName }}</i></li>
-                            <li class="list-group-item">Date Received: <i>{{ date_format($altdata['trackings'][$key]->created_at,'M d Y h:i')}}</i></li>
-                            @if($altdata['trackings'][$key]->action == 2)
-                            <p><li class="list-group-item">Status: <i>In Circulation</i></p>
+                            <hr style="border: 3px solid">
+
+                            <Label>Options</Label>
+                            @if($light->action == 2)
+                                <button type="button" class="btn btn-secondary" disabled><a href="{{ url('forward/'.$data->referenceNo) }}">Forward</a></button>
+                                <button type="button" class="btn btn-success"><a href="{{ url('receive/'.$data->referenceNo) }}">Receive</a></button>
                             @endif
-                            @if($altdata['trackings'][$key]->action == 1)
-                                <p><li class="list-group-item">Status: <i>On Hold</i></p>
+                            @if($light->action == 1)
+                                <button type="button" class="btn btn-primary"><a href="{{ url('forward/'.$data->referenceNo) }}">Forward</a></button>
+                                <button type="button" class="btn btn-secondary" disabled><a href="{{ url('receive/'.$data->referenceNo) }}">Receive</a></button>
                             @endif
-                        @endif
-                        @if( $altdata['trackings'][$key]->action == 2)
-                        <h5>Forwarded by {{ $altdata['trackings'][$key]->receiverName }}</h5>
-                            <li class="list-group-item">Forwarded to: <i>{{ $altdata['trackings'][$key]->officeName }}</i></li>
-                            <li class="list-group-item">Date Forwarded: <i>{{ date_format($altdata['trackings'][$key]->created_at,'M d Y h:i')}}</i></li>
-                            <li class="list-group-item">Forwarded from: <p><b>{{ $altdata['trackings'][$key]->prevReceiver }}</b> - <i>{{ $altdata['prev'][$key]->officeName }}</i></p></li>
-                            @if($altdata['trackings'][$key]->action == 2)
-                            <p><li class="list-group-item">Status: <i>In Circulation</i></p>
-                            @endif
-                            @if($altdata['trackings'][$key]->action == 1)
-                                <p><li class="list-group-item">Status: <i>On Hold</i></p>
-                            @endif
-                            </li>
-                        @endif
-                            </li>
                         </div>
-                    </ul>
-                    @endforeach
+                        </div>
+
+                <div class="float-child">
+                <div class="col-xs-7 col-sm-6 col-lg-8">
+                    <h3>Tracking Information</h3>
+                    <div class="card" style="width: 30rem;">
+                        @foreach($altdata['prev'] as $key => $prev)
+                        <ul class="list-group list-group-flush">
+                        <div class="section-header">
+                            <li>
+                            @if( $altdata['trackings'][$key]->action == 1)
+                            <h5>Received by {{ $altdata['trackings'][$key]->receiverName }}</h5>
+                                <li class="list-group-item">Office: <i>{{ $altdata['trackings'][$key]->officeName }}</i></li>
+                                <li class="list-group-item">Date Received: <i>{{ date_format($altdata['trackings'][$key]->created_at,'M d Y h:i')}}</i></li>
+                                @if($altdata['trackings'][$key]->action == 2)
+                                <p><li class="list-group-item">Status: <i>In Circulation</i></p>
+                                @endif
+                                @if($altdata['trackings'][$key]->action == 1)
+                                    <p><li class="list-group-item">Status: <i>On Hold</i></p>
+                                @endif
+                            @endif
+                            @if( $altdata['trackings'][$key]->action == 2)
+                            <h5>Forwarded by {{ $altdata['trackings'][$key]->receiverName }}</h5>
+                                <li class="list-group-item">Forwarded to: <i>{{ $altdata['trackings'][$key]->officeName }}</i></li>
+                                <li class="list-group-item">Date Forwarded: <i>{{ date_format($altdata['trackings'][$key]->created_at,'M d Y h:i')}}</i></li>
+                                <li class="list-group-item">Forwarded from: <p><b>{{ $altdata['trackings'][$key]->prevReceiver }}</b> - <i>{{ $altdata['prev'][$key]->officeName }}</i></p></li>
+                                @if($altdata['trackings'][$key]->action == 2)
+                                <p><li class="list-group-item">Status: <i>In Circulation</i></p>
+                                @endif
+                                @if($altdata['trackings'][$key]->action == 1)
+                                    <p><li class="list-group-item">Status: <i>On Hold</i></p>
+                                @endif
+                                </li>
+                            @endif
+                                </li>
+                            </div>
+                        </ul>
+                        @endforeach
+                    </div>
                 </div>
             </div>
+            </div>
         </div>
+    </div>
     </body>
 </html>
