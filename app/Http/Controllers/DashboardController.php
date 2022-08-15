d5da07c082643a1eead479847d59fd700d7b5694
Kidobashi <daryldon1abril@gmail.com>
2022-08-16 02:39:28 +0800

    82nd commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,8 @@ class DashboardController extends Controller
     {
         $totalDocs = Documents::all()->count();
         //Tagged Status Finished/Received By Intended User
+        $sentBack = Documents::where('status', 3)->count();
         //Sent Back/Status 3
-        return view('dashboard')->with(['totalDocs' => $totalDocs]);
+        return view('dashboard')->with(['totalDocs' => $totalDocs])->with('sentBack', $sentBack);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,7 +25,7 @@
               <div class="numbers">
                 <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Documents Sent Back</p>
                 <h5 class="font-weight-bolder mb-0">
-                  #######
+                    {{ $sentBack }}
                   {{-- <span class="text-success text-sm font-weight-bolder">+55%</span> --}}
                 </h5>
               </div>
