1b0b7e765a5620ee8ad1d2170f1d2fc3df18e6ae
Kidobashi <daryldon1abril@gmail.com>
2022-08-17 23:36:09 +0800

    84th commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,36 +9,73 @@
           <div class="row">
             <div class="col-8">
               <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Documents</p>
                 <h5 class="font-weight-bolder mb-0">
-                    {{ $totalDocs }}
-                  {{-- <span class="text-success text-sm font-weight-bolder">+55%</span> --}}
-                </h5>
-              </div>
-              <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Tagged Documents</p>
-                <h5 class="font-weight-bolder mb-0">
-                  {{ $taggedDocs }}
-                  {{-- <span class="text-success text-sm font-weight-bolder">+55%</span> --}}
-                </h5>
-              </div>
-              <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Documents Sent Back</p>
-                <h5 class="font-weight-bolder mb-0">
-                    {{ $sentBack }}
-                  {{-- <span class="text-success text-sm font-weight-bolder">+55%</span> --}}
+                    Documents Summary
                 </h5>
               </div>
             </div>
-            <div class="col-4 text-end">
-              <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+          </div>
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                    <div class="numbers">
+                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Documents</p>
+                        <h5 class="font-weight-bolder mb-0">
+                            {{ $totalDocs }}
+                        </h5>
+                      </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-archive-2 text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Tagged Documents</p>
+                      <h5 class="font-weight-bolder mb-0">
+                        {{ $taggedDocs }}
+                      </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-like-2 text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body p-3">
+              <div class="row">
+                <div class="col-8">
+                  <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">Total Documents Sent Back</p>
+                      <h5 class="font-weight-bolder mb-0">
+                          {{ $sentBack }}
+                      </h5>
+                  </div>
+                </div>
+                <div class="col-4 text-end">
+                  <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                    <i class="ni ni-fat-remove   text-lg opacity-10" aria-hidden="true"></i>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
     <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
